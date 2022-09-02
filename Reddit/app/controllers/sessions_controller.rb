a8a35c593e64840d15fb92d802d50a5cbf06cf88
darianchen <darianychen@gmail.com>
2022-09-02 10:37:18 -0400

    create user sign up and login

---
+++ Reddit/app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionController < ApplicationController
+
+    def new 
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_confidential(params[:user][:username], params[:user][:password])
+        if @user
+            login(@user)
+            redirect_to users_index
+        else
+            render :new
+        end
+    end
+
+    def destroy 
+        logout
+        redirect_to new_session_url
+    end
+
+
+
+
+end
---
+++ Reddit/app/models/user.rb
@@ -0,0 +1,47 @@
+class User < ApplicationRecord
+    #FIGVAPER
+
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6} allow_nil: true
+    attr_reader :password
+
+    def self.find_by_confidential(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+    
+    def is_password?(password)
+        BCrypt:Password.new(self.password_digest).is_password?(password)
+    end
+
+    def generate_unique_session_token
+        token = SecureRandom::urlsafe_base64
+
+        while User.exists?(session_token: token)
+             token = SecureRandom::urlsafe_base64
+        end
+        token
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+
+end
